797d34b57242e9c6b6ca8fa93aa96e9d6ca9a31e
Asiya Shaikh <104588842+asiya448@users.noreply.github.com>
2024-10-20 21:23:10 +0530

    inital commit

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-3xl text-green-500'>netflix gpt</p>
+    </div>
+  );
+}
+
+export default App;
